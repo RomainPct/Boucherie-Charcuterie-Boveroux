908ac672ba7e1f2db28f990f838bad655fc9c0b6
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-05 13:42:48 +0200

    start footer but have one problem

---
+++ pages/elements/footer.php
@@ -0,0 +1,14 @@
+<footer id="footer_container">
+    <div>
+        <h3>Boucherie Charcuterie Boveroux et Fils</h3>
+        <ul>
+            <li>Adresse : xx rue de la martinez</li>
+            <li>Tel : 07191891</li>
+            <li>Mail : hsuchieb@vhvhsq.co</li>
+        </ul>
+    </div>
+    <div>
+        <img src="#" alt="#">
+        <a href="#">Nous rendre visite via GoogleMaps</a>
+    </div>
+</footer>
