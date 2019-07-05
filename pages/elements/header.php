366932cbff9ae7a40baac8613e34fce6058df36a
HugoLeroyHL <47740185+HugoLeroyHL@users.noreply.github.com>
2019-07-05 23:18:20 +0200

    init header + push du soir
    On se retrouve demain matin pour mettre en beauté tout cela ;) Bonne nuit Hugo

---
+++ pages/elements/header.php
@@ -0,0 +1,18 @@
+<header class="header__container">
+    <div class="header__container__inside">
+        <div class="header__container__inside__logo">
+            
+        </div>
+        <nav class="header__container__inside__cat">
+            <div class="header__container__inside__cat__under">
+                <a href="">Qui sommes-nous ?</a>
+            </div>
+            <div class="header__container__inside__cat__under">
+                <a href="">Nos produits</a>
+            </div>
+            <div class="header__container__inside__cat__under">
+                <button>Mon Panier</button>
+            </div>
+        </nav>
+    </div>
+</header> 
