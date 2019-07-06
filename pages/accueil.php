db875911b2e98255c84bd7fcf72171ac4aca2d82
HugoLeroyHL <47740185+HugoLeroyHL@users.noreply.github.com>
2019-07-06 15:51:07 +0200

    fix button header

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,5 +1,18 @@
 <main class="home">
     <section class="home__section">
+        <div class="home__section__firstBlock">
+            <div class="home__section__firstBlock__title">
+                <h2>Boveroux & Fils</h2>
+            </div>
+            <div class="home__section__firstBlock__subtitle">
+                <h3>Boucherie Charcuterie</h3>
+            </div>
+            <div class="home__section__firstBlock__text">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, tempore quibusdam distinctio labore expedita exercitationem debitis odio impedit illum perferendis magnam, at nobis saepe, aliquam doloribus aut. Neque, aspernatur rerum?</p>
+            </div>
+            <div class="home__section__firstBlock__button">
 
+            </div>
+        </div>
     </section>
 </main>
