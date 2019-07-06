0cd5e80dffd9fef0f941affbbbe749752b7f2cdf
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-06 19:02:02 +0200

    add content candide footer

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -13,8 +13,7 @@ $c = new CandidePage('pied_de_page');
         </div>
     </div>
     <div class="footer__container__visite imageBox degradeLeftLigthDark filterBottomDark">
-        <img src="../../assets/images/photos/vaches_boveroux_2.jpg" alt="Vache boveroux">
-        <!-- src="php $c->image('image_du_pied_de_page',[1000,500]);" -->
-        <a href="#" title="Nous rendre visite via GoogleMaps"><?php $c->text('message_du_lien_vers_google_maps');?></a>
+        <img src="<?php $c->image('image_du_pied_de_page',[500,150]);?>" alt="Vache boveroux">
+        <a href="https://www.google.fr/maps/place/38+Rue+du+Pr%C3%A9sident+Poincar%C3%A9,+55100+Verdun/@49.1582801,5.384972,17z/data=!3m1!4b1!4m5!3m4!1s0x47eb1bff53310d53:0xbd541280c1e0ba4a!8m2!3d49.1582766!4d5.3871607" title="Nous rendre visite via GoogleMaps"><?php $c->text('message_du_lien_vers_google_maps');?></a>
     </div>
 </footer>
