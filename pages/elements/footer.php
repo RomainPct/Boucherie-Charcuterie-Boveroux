daa7b7153e994f347483559de30d9853958e7ba9
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-06 11:52:31 +0200

    try to fix my css

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -1,5 +1,5 @@
-<footer class="footer_container">
-    <div class="footer_container_adress">
+<footer class="footer__container">
+    <div class="footer__container__adress">
         <h3>Boucherie Charcuterie Boveroux et Fils</h3>
         <ul>
             <li>Adresse : xx rue de la martinez</li>
@@ -7,7 +7,7 @@
             <li>Mail : hsuchieb@vhvhsq.co</li>
         </ul>
     </div>
-    <div class="footer_container_visite">
+    <div class="footer__container__visite">
         <img src="#" alt="#">
         <a href="#">Nous rendre visite via GoogleMaps</a>
     </div>
