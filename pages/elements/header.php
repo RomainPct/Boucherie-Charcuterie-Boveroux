d96e07dad5b4b21fa06e8cb92ede4b7d18041d7f
HugoLeroyHL <47740185+HugoLeroyHL@users.noreply.github.com>
2019-07-06 19:12:53 +0200

    header fix name + position fix

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -1,17 +1,17 @@
-<header class="header__container">
-    <div class="header__container__inside">
-        <div class="header__container__inside__logo">
+<header class="headerContainer">
+    <div class="headerContainer__inside">
+        <div class="headerContainer__inside__logo">
             <img src="" alt="logo du site">
         </div>
-        <nav class="header__container__inside__cat">
-            <div class="header__container__inside__cat__under">
+        <nav class="headerContainer__inside__cat">
+            <div class="headerContainer__inside__cat__under">
                 <a href="">Qui sommes-nous ?</a>
             </div>
-            <div class="header__container__inside__cat__under">
+            <div class="headerContainer__inside__cat__under">
                 <a href="">Nos produits</a>
             </div>
-            <div class="header__container__inside__cat__underButton">
-                <button class="secondaryButton">Mon Panier</button>
+            <div class="headerContainer__inside__cat__underButton">
+                <a href="" title="" class="tertiaryButton">Mon Panier</a>
             </div>
         </nav>
     </div>
