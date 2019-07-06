a3c8710a4db187b4d8a031507695cbffa8bddc57
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-06 22:15:24 +0200

    footer

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -14,6 +14,6 @@ $c = new CandidePage('pied_de_page');
     </div>
     <div class="footer__container__visite imageBox degradeLeftLigthDark filterBottomDark">
         <img src="<?php $c->image('image_du_pied_de_page',[500,150]);?>" alt="Vache boveroux">
-        <a href="https://www.google.fr/maps/place/38+Rue+du+Pr%C3%A9sident+Poincar%C3%A9,+55100+Verdun/@49.1582801,5.384972,17z/data=!3m1!4b1!4m5!3m4!1s0x47eb1bff53310d53:0xbd541280c1e0ba4a!8m2!3d49.1582766!4d5.3871607" title="Nous rendre visite via GoogleMaps"><?php $c->text('message_du_lien_vers_google_maps');?></a>
+        <a href="https://www.google.fr/maps/place/Cauchon+Rach%C3%ABl/@49.3978757,4.6978363,17z/data=!3m1!4b1!4m5!3m4!1s0x47ea371b4b29cf45:0xb10e227341c7d9df!8m2!3d49.3978722!4d4.700025" title="Nous rendre visite via GoogleMaps"><?php $c->text('message_du_lien_vers_google_maps');?></a>
     </div>
 </footer>
