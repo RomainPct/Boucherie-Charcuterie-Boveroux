bceb4f50ce83e5ccd471146fe54ebb3fd8874bb0
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-06 15:59:56 +0200

    add candide on footer

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -1,14 +1,18 @@
+<?php
+$c = new CandidePage('footer');
+?>
 <footer class="footer__container">
     <div class="footer__container__adress">
-        <h4>Boucherie Charcuterie Boveroux et Fils</h4>
+        <h4><?php $c->text('nom_entreprise');?></h4>
         <ul>
-            <li>Adresse : xx rue de la martinez</li>
-            <li>Tel : 07191891</li>
-            <li>Mail : hsuchieb@vhvhsq.co</li>
+            <li><?php $c->text('ligne_numero_1');?></li>
+            <li><?php $c->text('ligne_numero_2');?></li>
+            <li><?php $c->text('ligne_numero_3');?></li>
         </ul>
     </div>
     <div class="footer__container__visite imageBox degradeLeftLigthDark filterBottomDark">
         <img src="../../assets/images/photos/vaches_boveroux_2.jpg" alt="Vache boveroux">
-        <a href="#" title="Nous rendre visite via GoogleMaps">Nous rendre visite via GoogleMaps</a>
+        <!-- src="php $c->image('image_du_footer',[1000,500]);" -->
+        <a href="#" title="Nous rendre visite via GoogleMaps"><?php $c->text('nom_lien_vers_google_maps');?></a>
     </div>
 </footer>
