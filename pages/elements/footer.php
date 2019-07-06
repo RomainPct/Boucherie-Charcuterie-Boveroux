f3d9b01cc7b2ff48f3d431f623d4e67f49b31976
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-06 18:25:35 +0200

    fis candide name in footer + fix scss of the footer

--- pages/elements/footer.php
+++ pages/elements/footer.php
@@ -1,18 +1,20 @@
 <?php
-$c = new CandidePage('footer');
+$c = new CandidePage('pied_de_page');
 ?>
 <footer class="footer__container">
     <div class="footer__container__adress">
-        <h4><?php $c->text('nom_entreprise');?></h4>
-        <ul>
-            <li><?php $c->text('ligne_numero_1');?></li>
-            <li><?php $c->text('ligne_numero_2');?></li>
-            <li><?php $c->text('ligne_numero_3');?></li>
-        </ul>
+        <div class="footer__container__adress__list">
+            <h4><?php $c->text('nom_entreprise');?></h4>
+            <ul>
+                <li><?php $c->text('adresse');?></li>
+                <li><?php $c->text('téléphone');?></li>
+                <li><?php $c->text('mail');?></li>
+            </ul>
+        </div>
     </div>
     <div class="footer__container__visite imageBox degradeLeftLigthDark filterBottomDark">
         <img src="../../assets/images/photos/vaches_boveroux_2.jpg" alt="Vache boveroux">
-        <!-- src="php $c->image('image_du_footer',[1000,500]);" -->
-        <a href="#" title="Nous rendre visite via GoogleMaps"><?php $c->text('nom_lien_vers_google_maps');?></a>
+        <!-- src="php $c->image('image_du_pied_de_page',[1000,500]);" -->
+        <a href="#" title="Nous rendre visite via GoogleMaps"><?php $c->text('message_du_lien_vers_google_maps');?></a>
     </div>
 </footer>
