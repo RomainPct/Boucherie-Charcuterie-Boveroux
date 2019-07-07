b71d65c9e8ed7bc1f43e5fb6ecc3fcf254d86e3b
HugoLeroyHL <47740185+HugoLeroyHL@users.noreply.github.com>
2019-07-07 23:28:28 +0200

    fix spacing, class

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,24 +1,16 @@
 <main class="home__main">
     <section class="home__section">
-            <div class="class="home__section__firstBlock>
-                <div class="imageBox degradeBottomDark filterBottomDark home__section__firstBlock__img">
-                    <img src="../assets/images/photos/vaches_boveroux_7.jpg" alt="">
+                <div class="imageBox degradeBottomDark filterBottomDark home__section__img">
+                    <img src="../assets/images/photos/vaches_boveroux_7.jpg" alt="images de vaches">
                 </div>
-                <div class="home__section__firstBlock__textBlock">
-                    <div class="home__section__firstBlock__textBlock__title">
+                <div class="home__section__textBlock">
                         <h2>Boveroux & Fils</h2>
-                    </div>
-                    <div class="home__section__firstBlock__textBlock__subtitle">
+                    <div class="home__section__textBlock__text">
                         <h3>Boucherie Charcuterie</h3>
-                    </div>
-                    <div class="home__section__firstBlock__textBlock__text">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, tempore quibusdam distinctio labore expedita exercitationem debitis odio impedit illum perferendis magnam, at nobis saepe, aliquam doloribus aut. Neque, aspernatur rerum?</p>
-                    </div>
-                    <div class="home__section__firstBlock__textBlock__button">
+                        <p class="home__section__textBlock__text__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, tempore quibusdam distinctio labore expedita exercitationem debitis odio impedit illum perferendis magnam, at nobis saepe, aliquam doloribus aut. Neque, aspernatur rerum?</p>
                         <a href="produits" title="bouton vers produits" class="primaryButton">Découvrir les produits</a>
+                    </div>
                 </div>
-            </div>
-        </div>
         <div class="home__section__secondBlock">
             <div class="home__section__secondBlock__leftDiv">
                 <div class="home__section__secondBlock__leftDiv__title">
@@ -41,7 +33,7 @@
             </div>
             <div class="home__section__secondBlock__rightDiv">
                 <h3>Notre histoire</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus vitae quisquam dicta accusamus tempore. Commodi odio tempore distinctio, excepturi ad necessitatibus dicta illum nam eius quasi dolor inventore ut voluptatum.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus vitae quisquam dicta accusamus tempore. Commodi odio tempore distinctio, excepturi ad necessitatibus dicta illum nam eius quasi dolor inventore ut voluptatum amet consectetur adipisicing elit. Vel odit similique iusto assumenda? Deserunt nesciunt delectus suscipit voluptatum nulla odit, quod quo dolor obcaecati a. Placeat eaque iusto iste sint. </p>
                 <a href="quisommesnous" title="Bouton pour en apprendre plus" class="secondaryButton">En apprendre plus</a>
             </div>
         </div>
