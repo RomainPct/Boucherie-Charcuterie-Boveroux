da817f675581025045dfa61b703e683c7908a2f5
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-07 17:13:56 +0200

    html quisommesnous.php

---
+++ pages/quisommesnous.php
@@ -0,0 +1,33 @@
+<main class="whoweare__container">
+    <h1>Qui sommes nous ?</h1>
+    <div class="whoweare__container__line1">
+        <div class="whoweare__container__line1__left">
+            <h2>Une histoire de famille</h2>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
+        </div>
+        <div class="whoweare__container__line1__right">
+            <img src="../assets/images/photos/vaches_boveroux_9.jpg" alt="Vaches boveroux">
+        </div>
+    </div>
+    <div class="whoweare__container__line2">
+        <div class="whoweare__container__line2__left">
+            <img src="../assets/images/icones/bio.png" alt="Logo agriculture biologique">
+        </div>
+        <div class="whoweare__container__line2__right">
+            <h3>Production bio</h3>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum.</p>
+        </div>
+    </div>
+    <div class="whoweare__container__line3">
+        <div class="whoweare__container__line3__left">
+            <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="Viande de boeuf boveroux">
+        </div>
+        <div class="whoweare__container__line3__right">
+            <h3>La qualité au coeur de notre travail</h3>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
+        </div>
+    </div>
+    <div class="whoweare__container__btnline">
+        <a class="primaryButton" href="produits" title="Découvrir nos produits">Découvrir nos produits</a>
+    </div>
+</main>
