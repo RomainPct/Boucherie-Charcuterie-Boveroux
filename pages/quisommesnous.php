80a7cbfbdbb75bb8b2d3efa08364f44f7bd2a2d2
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-07 23:31:12 +0200

    finish html / css quisommesnous.php

--- pages/quisommesnous.php
+++ pages/quisommesnous.php
@@ -5,7 +5,7 @@
             <h2>Une histoire de famille</h2>
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
         </div>
-        <div class="whoweare__container__line1__right">
+        <div class="whoweare__container__line1__right imageBox filterBottomDark degradeLeftDark">
             <img src="../assets/images/photos/vaches_boveroux_9.jpg" alt="Vaches boveroux">
         </div>
     </div>
@@ -19,7 +19,7 @@
         </div>
     </div>
     <div class="whoweare__container__line3">
-        <div class="whoweare__container__line3__left">
+        <div class="whoweare__container__line3__left imageBox filterBottomDark degradeRightDark">
             <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="Viande de boeuf boveroux">
         </div>
         <div class="whoweare__container__line3__right">
@@ -27,7 +27,7 @@
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
         </div>
     </div>
-    <div class="whoweare__container__btnline">
+    <div class="whoweare__container__lineBtn">
         <a class="primaryButton" href="produits" title="Découvrir nos produits">Découvrir nos produits</a>
     </div>
 </main>
