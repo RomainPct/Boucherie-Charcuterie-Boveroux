dab8b7e0a6706d98e3d0a57001c0379ee7acd0d9
HugoLeroyHL <47740185+HugoLeroyHL@users.noreply.github.com>
2019-07-08 22:09:48 +0200

    nav & title prod

---
+++ pages/produits.php
@@ -0,0 +1,14 @@
+<main>
+    <section class="prod__section">
+        <h2>Nos produits</h2>
+        <nav>
+            <a href="" class="secondaryButton">Tous</a>
+            <a href="" class="tertiaryButton">Charcuterie</a>
+            <a href="" class="tertiaryButton">Porc</a>
+            <a href="" class="tertiaryButton">Veau</a>
+            <a href="" class="tertiaryButton">Boeuf</a>
+            <a href="" class="tertiaryButton">Plats cuisinés</a>
+            <a href="" class="primaryButton">Valider ma commande</a>
+        </nav>
+    </section>
+</main>
