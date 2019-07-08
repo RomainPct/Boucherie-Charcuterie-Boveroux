0a28a8c9ce11a7ea0cb585cfe37bb0f422be4a08
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-08 11:51:15 +0200

    upgrade class name in quisommesnous.php

--- pages/quisommesnous.php
+++ pages/quisommesnous.php
@@ -1,33 +1,33 @@
 <main class="whoweare__container">
     <h1>Qui sommes nous ?</h1>
-    <div class="whoweare__container__line1">
-        <div class="whoweare__container__line1__left">
+    <div class="whoweare__container__history">
+        <div class="whoweare__container__history__text">
             <h2>Une histoire de famille</h2>
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
         </div>
-        <div class="whoweare__container__line1__right imageBox filterBottomDark degradeLeftDark">
+        <div class="whoweare__container__history__picture imageBox filterBottomDark degradeLeftDark">
             <img src="../assets/images/photos/vaches_boveroux_9.jpg" alt="Vaches boveroux">
         </div>
     </div>
-    <div class="whoweare__container__line2">
-        <div class="whoweare__container__line2__left">
+    <div class="whoweare__container__bioProd">
+        <div class="whoweare__container__bioProd__picture">
             <img src="../assets/images/icones/bio.png" alt="Logo agriculture biologique">
         </div>
-        <div class="whoweare__container__line2__right">
+        <div class="whoweare__container__bioProd__text">
             <h3>Production bio</h3>
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum.</p>
         </div>
     </div>
-    <div class="whoweare__container__line3">
-        <div class="whoweare__container__line3__left imageBox filterBottomDark degradeRightDark">
+    <div class="whoweare__container__quality">
+        <div class="whoweare__container__quality__picture imageBox filterBottomDark degradeRightDark">
             <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="Viande de boeuf boveroux">
         </div>
-        <div class="whoweare__container__line3__right">
+        <div class="whoweare__container__quality__text">
             <h3>La qualité au coeur de notre travail</h3>
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
         </div>
     </div>
-    <div class="whoweare__container__lineBtn">
+    <div class="whoweare__container__button">
         <a class="primaryButton" href="produits" title="Découvrir nos produits">Découvrir nos produits</a>
     </div>
 </main>
