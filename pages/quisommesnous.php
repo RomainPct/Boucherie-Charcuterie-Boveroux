e94119b1b6b3c4f40af3168653289357ad8ec870
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-08 11:58:15 +0200

    add candide quisommesnous.php

--- pages/quisommesnous.php
+++ pages/quisommesnous.php
@@ -1,12 +1,15 @@
+<?php
+$c = new CandidePage('qui_sommes_nous')
+?>
 <main class="whoweare__container">
     <h1>Qui sommes nous ?</h1>
     <div class="whoweare__container__history">
         <div class="whoweare__container__history__text">
-            <h2>Une histoire de famille</h2>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
+            <h2><?php $c->text('titre_partie_histoire');?></h2>
+            <p><?php $c->text('texte_partie_histoire');?></p>
         </div>
         <div class="whoweare__container__history__picture imageBox filterBottomDark degradeLeftDark">
-            <img src="../assets/images/photos/vaches_boveroux_9.jpg" alt="Vaches boveroux">
+            <img src="<?php $c->image('image_partie_histoire',[1000,500]);?>" alt="Vaches boveroux">
         </div>
     </div>
     <div class="whoweare__container__bioProd">
@@ -14,20 +17,20 @@
             <img src="../assets/images/icones/bio.png" alt="Logo agriculture biologique">
         </div>
         <div class="whoweare__container__bioProd__text">
-            <h3>Production bio</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum.</p>
+            <h3><?php $c->text('titre_partie_production_bio');?></h3>
+            <p><?php $c->text('texte_partie_production_bio');?></p>
         </div>
     </div>
     <div class="whoweare__container__quality">
         <div class="whoweare__container__quality__picture imageBox filterBottomDark degradeRightDark">
-            <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="Viande de boeuf boveroux">
+            <img src="<?php $c->image('image_partie_production_bio',[500,250]);?>" alt="Viande de boeuf boveroux">
         </div>
         <div class="whoweare__container__quality__text">
-            <h3>La qualité au coeur de notre travail</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore</p>
+            <h3><?php $c->text('titre_partie_qualite');?></h3>
+            <p><?php $c->text('texte_partie_qualite');?></p>
         </div>
     </div>
     <div class="whoweare__container__button">
-        <a class="primaryButton" href="produits" title="Découvrir nos produits">Découvrir nos produits</a>
+        <a class="primaryButton" href="produits" title="Découvrir nos produits"><?php $c->text('texte_du_bouton_qui_renvoie_vers_la_page_produit');?></a>
     </div>
 </main>
