0e30d9fa3b172f12d5cee297156faefcc3be4401
Tina Elh <tinaelha@gmail.com>
2019-07-09 13:52:29 +0200

    correction erreurs console basket

--- src/scripts/1_basket_class.js
+++ src/scripts/1_basket_class.js
@@ -8,12 +8,12 @@ class Basket {
         this.cleanBasket()
         this.addNewProduct(25,'andouillette',50)
         this.addNewProduct(15,'viande',200)
-        console.log(this.basket[25])
-        console.log(this.basket[15])
+       // console.log(this.basket[25])
+       // console.log(this.basket[15])
         this.removeAProduct(25)
-        console.log(this.basket[25])
+      // console.log(this.basket[25])
         this.getTotalPrice()
-        console.log(this.basket[15])
+      // console.log(this.basket[15])
     }
 
     saveBasketOnLocalStorage(){
@@ -35,32 +35,32 @@ class Basket {
     }
 
     addNewProduct(id,name,price){
+        console.log("nouveau produit" +" "+ this.basket[id])
         //Ajouter un produit au panier
-        this.basket[id] = [name, price, 1]
-        console.log(this.basket)
         // Si deja dans le panier -> augmenter la quantité de 1
         if (this.basket[id] != null){
             this.basket[id][2] += 1
-            console.log(this.basket[id][2])
+            console.log("this.basket[id] != null" + this.basket[id][2])
         } else{
             this.basket[id] = [name, price, 1]
-            console.log(this.basket[id])
+            console.log("else :" + this.basket[id])
         }
         // Enregistrer en localsorage le nouveau panier
         this.saveBasketOnLocalStorage()
     }
 
     removeAProduct(id){
+        console.log("supp un produit" +" "+ this.basket[id])
         // Diminuer la quantité du produit dans le panier
-        this.basket[id][2] -= 1
         if (this.basket[id]!= null){
             this.basket[id][2] -= 1
-            console.log(this.basket[id][2])
+            console.log("this.basket[id]!= null:" + this.basket[id][2])
         } 
         // Si nouvelle quantité = 0 -> supprimer du panier
+        console.log("avant second if" +" "+ this.basket[id])
         if(this.basket[id][2]== 0){
-            this.basket[id] = null
-            console.log(this.basket[id])
+            delete this.basket[id]
+            console.log("this.basket[id][2]== 0 :" + this.basket[id])
         }
         // Enregistrer en localsorage le nouveau panier
         this.saveBasketOnLocalStorage()
@@ -72,7 +72,7 @@ class Basket {
         for (let i in this.basket){
             totalPrice += ((this.basket[i][1])*(this.basket[i][2]));
             // les valeurs prix et quantité pas reconnues car pas tableau id ?? trisfristefte
-            console.log(totalPrice)
+            console.log("résultat du prix total :" + totalPrice)
         }
         return totalPrice
     }
@@ -80,6 +80,6 @@ class Basket {
 }
 
 const basket = new Basket()
-//console.log(basket.addNewProduct())
-console.log(basket.removeAProduct())
+console.log(basket.addNewProduct(25))
+console.log(basket.removeAProduct(25))
 console.log(basket.getTotalPrice()) 
