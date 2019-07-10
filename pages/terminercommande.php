7a82b72bb2c501dfd8884bf840a95a4abdf0cd90
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-10 16:32:42 +0200

    start html / css terminercommande page

---
+++ pages/terminercommande.php
@@ -0,0 +1,30 @@
+<main class="finishOrder__container">
+    <h1>Terminer ma commande</h1>
+    <div class="finishOrder__container__left">
+        <h2>Terminer ma commande</h2>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem.</p>
+    </div>
+    <div class="finishOrder__container__right">
+        <form action="" method="post" class="finishOrder__container____right__form">
+            <div>
+                <label for="POST-name">Votre nom et prénom</label>
+                <input type="text" name="name" id="POST-name" required>
+            </div>
+            <div>
+                <label for="POST-email">Votre adresse email</label>
+                <input type="email" name="email" id="POST-email" required>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <label for="POST-message">Un petit message pour nous ?</label>
+                <input type="text" name="message" id="POST-message" required>
+            </div>
+            <div>
+                <input type="submit" value="Envoyer ma commande">
+            </div>
+        </form>
+    </div>
+</main>
