3fc4b5bb5a2e65db3abbf6abfb0f89e1bf055431
Tina Elh <tinaelha@gmail.com>
2019-07-10 13:49:46 +0200

    début html et scss page produits détaillés

---
+++ pages/produitsdetailles.php
@@ -0,0 +1,10 @@
+<div class="container__detailed__products">
+    <div class="container__detailed__products__informations">
+        <div class="container__detailed__products__informations__image">
+            <!-- Mettre image correspondante-->
+        </div>
+        <div class="container__detailed__products__informations__text">
+            <!-- Description du produit et bouton "Ajouter au panier"-->
+        </div>
+    </div>
+</div>
