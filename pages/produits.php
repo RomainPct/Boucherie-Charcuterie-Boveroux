db19285a92c75db5c6efc73967b09c8051660482
Hugo Leroy <hugo.leroy15@gmail.com>
2019-07-10 14:12:17 +0200

    categories produits html css

--- pages/produits.php
+++ pages/produits.php
@@ -1,14 +1,16 @@
 <main>
     <section class="prod__section">
         <h2>Nos produits</h2>
-        <nav>
-            <a href="" class="secondaryButton">Tous</a>
-            <a href="" class="tertiaryButton">Charcuterie</a>
-            <a href="" class="tertiaryButton">Porc</a>
-            <a href="" class="tertiaryButton">Veau</a>
-            <a href="" class="tertiaryButton">Boeuf</a>
-            <a href="" class="tertiaryButton">Plats cuisinés</a>
-            <a href="" class="primaryButton">Valider ma commande</a>
-        </nav>
+        <div class="prod__section__cat">
+            <div>
+                <a href="" class="secondaryButton">Tous</a>
+                <a href="" class="tertiaryButton">Charcuterie</a>
+                <a href="" class="tertiaryButton">Porc</a>
+                <a href="" class="tertiaryButton">Veau</a>
+                <a href="" class="tertiaryButton">Boeuf</a>
+                <a href="" class="tertiaryButton">Plats cuisinés</a>
+            </div>
+                <a href="" class="primaryButton">Valider ma commande</a>
+        </div>
     </section>
 </main>
