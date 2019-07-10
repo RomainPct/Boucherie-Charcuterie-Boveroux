763c83ab3a67c1f46e901b8a7303a824426baf3b
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-10 22:35:01 +0200

    page terminermacommande without summary

--- pages/terminercommande.php
+++ pages/terminercommande.php
@@ -5,25 +5,25 @@
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem.</p>
     </div>
     <div class="finishOrder__container__right">
-        <form action="" method="post" class="finishOrder__container____right__form">
-            <div>
+        <form action="" method="post" class="finishOrder__container__right__form">
+            <div class="finishOrder__container__right__form__name">
                 <label for="POST-name">Votre nom et prénom</label>
                 <input type="text" name="name" id="POST-name" required>
             </div>
-            <div>
+            <div class="finishOrder__container__right__form__email">
                 <label for="POST-email">Votre adresse email</label>
                 <input type="email" name="email" id="POST-email" required>
             </div>
-            <div>
-                <div></div>
+            <div class="finishOrder__container__right__form__summary">
+                <p>Résumé de votre commande</p>
                 <div></div>
             </div>
-            <div>
+            <div class="finishOrder__container__right__form__message">
                 <label for="POST-message">Un petit message pour nous ?</label>
                 <input type="text" name="message" id="POST-message" required>
             </div>
-            <div>
-                <input type="submit" value="Envoyer ma commande">
+            <div class="finishOrder__container__right__form__send">
+                <a class="primaryButton" href="#" title="Envoyer ma commande">Envoyer ma commande</a>
             </div>
         </form>
     </div>
