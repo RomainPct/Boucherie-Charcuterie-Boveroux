f7f64ba509ba3ab2bbcb33bd0902ee68e84d4166
Tina Elh <tinaelha@gmail.com>
2019-07-10 22:50:38 +0200

    debut page produits détaillés

--- pages/produitsdetailles.php
+++ pages/produitsdetailles.php
@@ -1,10 +1,18 @@
 <div class="containerDetailedProducts">
-    <div class="containerDetailedProducts__informations">
-        <div class="containerDetailedProducts__informations__image">
-            <!-- Mettre image correspondante-->
+    <div class="containerDetailedProducts__informationsDetailed">
+        <div class="containerDetailedProducts__informationsDetailed__imageDetailed">
+            <!-- Mettre image correspondante -->
+            <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="photo de boeuf"> 
         </div>
-        <div class="containerDetailedProducts__informations__text">
+        <div class="containerDetailedProducts__informationsDetailed__textDetailed">
             <!-- Description du produit et bouton "Ajouter au panier"-->
+            <div class="containerDetailedProducts__informationsDetailed__textDetailed__descriptionDetailed">
+                <h3>Andouillettes</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint nulla aperiam perspiciatis aliquid omnis esse, eius quibusdam sapiente recusandae totam sunt iusto ea numquam ipsam maxime, laborum exercitationem sit reiciendis!</p>
+            </div>
+            <div class="containerDetailedProducts__informationsDetailed__textDetailed__buttonDetailed">
+                <a title="bouton ajouter panier" class="primaryButton" href="#">Ajouter au panier</a>
+            </div>
         </div>
     </div>
 </div>
