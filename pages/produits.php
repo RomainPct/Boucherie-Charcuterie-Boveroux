72462936fbb09644410a2291270f03b0a8ae362a
HugoleroyHL <hugo.leroy15@gmail.com>
2019-07-12 15:03:09 +0200

    don't touch candide

--- pages/produits.php
+++ pages/produits.php
@@ -13,13 +13,13 @@ $c1 = new CandideCollection('produits');
     <section class="prodSection">
         <h2>Nos produits</h2>
         <div class="prodSection__cat">
-            <div>
-                <a href="" class="secondaryButton">Tous</a>
-                <a href="" class="tertiaryButton">Charcuterie</a>
-                <a href="" class="tertiaryButton">Porc</a>
-                <a href="" class="tertiaryButton">Veau</a>
-                <a href="" class="tertiaryButton">Boeuf</a>
-                <a href="" class="tertiaryButton">Plats cuisinés</a>
+            <div class="catlist">
+                <a href="" data-target="all" class="secondaryButton">Tous</a>
+                <a href="" data-target="charcuterie" class="tertiaryButton">Charcuterie</a>
+                <a href="" data-target="porc" class="tertiaryButton">Porc</a>
+                <a href="" data-target="veau" class="tertiaryButton">Veau</a>
+                <a href="" data-target="boeuf" class="tertiaryButton">Boeuf</a>
+                <a href="" data-target="plats_cuisines" class="tertiaryButton">Plats cuisinés</a>
             </div>
                 <a href="terminercommande" class="primaryButton">Valider ma commande</a>
         </div>
@@ -28,9 +28,69 @@ $c1 = new CandideCollection('produits');
             <?php
             foreach($c1->avalaibleItemIds()as $id){
                 ?>
-                <div class="prodSectionList__product" data-productid="19" data-producttype="boeuf">
+                <div class="prodSectionList__product all charcuterie" data-productid="<?php echo $id ?>" data-producttype="charcuterie">
                     <div class="prodSectionList__product__contain">
-                        <img src="<?php $c1->image('image',$id)?>" alt="image du produit">
+                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
+                        <p><?php $c1->text('nom_du_produit',$id)?></p>
+                        <p><?php $c1->text('prix',$id)?></p>
+                        <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
+                    </div>
+                </div>
+                <?php
+
+            }
+            ?>
+            <?php
+            foreach($c1->avalaibleItemIds()as $id){
+                ?>
+                <div class="prodSectionList__product all porc" data-productid="<?php echo $id ?>" data-producttype="porc">
+                    <div class="prodSectionList__product__contain">
+                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
+                        <p><?php $c1->text('nom_du_produit',$id)?></p>
+                        <p><?php $c1->text('prix',$id)?></p>
+                        <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
+                    </div>
+                </div>
+                <?php
+
+            }
+            ?>
+            <?php
+            foreach($c1->avalaibleItemIds()as $id){
+                ?>
+                <div class="prodSectionList__product all veau" data-productid="<?php echo $id ?>" data-producttype="veau">
+                    <div class="prodSectionList__product__contain">
+                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
+                        <p><?php $c1->text('nom_du_produit',$id)?></p>
+                        <p><?php $c1->text('prix',$id)?></p>
+                        <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
+                    </div>
+                </div>
+                <?php
+
+            }
+            ?>
+            <?php
+            foreach($c1->avalaibleItemIds()as $id){
+                ?>
+                <div class="prodSectionList__product all boeuf" data-productid="<?php echo $id ?>" data-producttype="boeuf">
+                    <div class="prodSectionList__product__contain">
+                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
+                        <p><?php $c1->text('nom_du_produit',$id)?></p>
+                        <p><?php $c1->text('prix',$id)?></p>
+                        <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
+                    </div>
+                </div>
+                <?php
+
+            }
+            ?>
+            <?php
+            foreach($c1->avalaibleItemIds()as $id){
+                ?>
+                <div class="prodSectionList__product all plats_cuisines" data-productid="<?php echo $id ?>" data-producttype="plats_cuisines">
+                    <div class="prodSectionList__product__contain">
+                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
                         <p><?php $c1->text('nom_du_produit',$id)?></p>
                         <p><?php $c1->text('prix',$id)?></p>
                         <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
---
+++ src/scripts/3_product.js
@@ -0,0 +1,27 @@
+class Filter {
+    constructor() {
+        this.buttons = document.querySelectorAll('.catlist a')
+        this.cards = document.querySelectorAll('.prodSectionList')
+
+        this.init()
+    }
+
+    init() {
+        for (let i = 0; i < this.buttons.length; i++) {
+            this.buttons[i].addEventListener('click', () => {
+                let target = this.buttons[i].getAttribute('data-target');
+
+                for (let j = 0; j < this.cards.length; j++) {
+                    if (this.cards[j].classList.contains(target)) {
+                        this.cards[j].style.display = 'block'
+                    } else {
+                        this.cards[j].style.display = 'none'
+                    }
+                }
+            })
+        }
+    }
+
+}
+
+let myFilter = new Filter()
