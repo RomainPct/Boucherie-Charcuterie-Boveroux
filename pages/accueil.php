61ad9955e6e7df4efccc072ae7d4ef59d185f00e
HugoleroyHL <hugo.leroy15@gmail.com>
2019-07-14 14:38:02 +0200

    fix bug img Home

--- pages/accueil.php
+++ pages/accueil.php
@@ -3,11 +3,7 @@ $c = new CandidePage('accueil')
 ?>
 <main class="home__main">
     <section class="home__section">
-        <div class="home__section__textandimg">
-            <div class="imageBox degradeBottomDark filterBottomDark home__section__textandimg__img">
-                <img src="../assets/images/photos/vaches_boveroux_7.jpg" alt="images de vaches">
-                </div>
-                <div class="home__section__textandimg__textBlock">
+                        <div class="home__section__textandimg__textBlock">
                         <h2><?php $c->text('titre');?></h2>
                     <div class="home__section__textandimg__textBlock__text">
                         <h3><?php $c->text('sous_titre');?></h3>
@@ -15,6 +11,11 @@ $c = new CandidePage('accueil')
                         <a href="produits" title="bouton vers produits" class="primaryButton">Découvrir les produits</a>
                     </div>
                 </div>
+        <div class="home__section__textandimg imageBox degradeBottomDark filterBottomDark">
+            <div class="home__section__textandimg__img">
+                <img src="../assets/images/photos/vaches_boveroux_7.jpg" alt="images de vaches">
+                </div>
+
         </div>
         <div class="home__section__secondBlock">
             <div class="home__section__secondBlock__leftDiv">
