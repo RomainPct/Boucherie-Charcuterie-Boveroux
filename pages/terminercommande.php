9c6017ee633974f8a3e3b086204e35ea66823321
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-16 15:58:25 +0200

    add html/css summary in terminercommande.php

--- pages/terminercommande.php
+++ pages/terminercommande.php
@@ -19,10 +19,28 @@ $c = new CandidePage('terminer_ma_commande')
                 <input type="email" name="email" id="POST-email">
                 <span></span>
             </div>
+
+
             <div class="finishOrder__container__right__form__summary">
                 <p>Résumé de votre commande</p>
-                <div></div>
+                <div class="finishOrder__container__right__form__summary__summaryBox">
+                    <div class="finishOrder__container__right__form__summary__summaryBox__line">
+                        <div class="finishOrder__container__right__form__summary__summaryBox__line__product">Poitrine de veau</div>
+                        <div class="finishOrder__container__right__form__summary__summaryBox__line__spec">
+                            <div class="finishOrder__container__right__form__summary__summaryBox__line__spec__price">29€</div>
+                            <a class="finishOrder__container__right__form__summary__summaryBox__line__spec__minus primaryButton" href="#" title="moins">-</a>
+                            <div class="finishOrder__container__right__form__summary__summaryBox__line__spec__quantity">1</div>
+                            <a class="finishOrder__container__right__form__summary__summaryBox__line__spec__more primaryButton" href="#" title="plus">+</a>
+                        </div>
+                    </div>
+                    <div class="finishOrder__container__right__form__summary__summaryBox__lineTotalCost">
+                        <div>Total : 98€</div>
+                    </div>
+                </div>
             </div>
+
+
+
             <div class="finishOrder__container__right__form__message">
                 <label for="POST-message">Un petit message pour nous ?</label>
                 <textarea type="text" name="message" id="POST-message"></textarea>
