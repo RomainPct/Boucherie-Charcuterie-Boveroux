ac87e1446455dc623daa173beafc6f58d5fe4018
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-07-18 15:10:51 +0200

    try to start of the summary display

--- pages/terminercommande.php
+++ pages/terminercommande.php
@@ -19,12 +19,11 @@ $c = new CandidePage('terminer_ma_commande')
                 <input type="email" name="email" id="POST-email">
                 <span></span>
             </div>
-
-
             <div class="finishOrder__container__right__form__summary">
                 <p>Résumé de votre commande</p>
                 <div class="finishOrder__container__right__form__summary__summaryBox">
-                    <div class="finishOrder__container__right__form__summary__summaryBox__line">
+
+                    <!-- <div class="finishOrder__container__right__form__summary__summaryBox__line">
                         <div class="finishOrder__container__right__form__summary__summaryBox__line__product">Poitrine de veau</div>
                         <div class="finishOrder__container__right__form__summary__summaryBox__line__spec">
                             <div class="finishOrder__container__right__form__summary__summaryBox__line__spec__price">29€</div>
@@ -32,15 +31,13 @@ $c = new CandidePage('terminer_ma_commande')
                             <div class="finishOrder__container__right__form__summary__summaryBox__line__spec__quantity">1</div>
                             <a class="finishOrder__container__right__form__summary__summaryBox__line__spec__more primaryButton" href="#" title="plus">+</a>
                         </div>
-                    </div>
+                    </div> -->
+
                     <div class="finishOrder__container__right__form__summary__summaryBox__lineTotalCost">
                         <div>Total : 98€</div>
                     </div>
                 </div>
             </div>
-
-
-
             <div class="finishOrder__container__right__form__message">
                 <label for="POST-message">Un petit message pour nous ?</label>
                 <textarea type="text" name="message" id="POST-message"></textarea>
--- src/scripts/6_confirmOrder.js
+++ src/scripts/6_confirmOrder.js
@@ -3,3 +3,38 @@ basket.addProduct("0_boeuf","Steack",29)
 basket.addProduct("2_porc","Côte de porc",19)
 basket.addProduct("2_porc","Côte de porc",19)
 basket.addProduct("0_plats_cuisines","Ratatouille",7)
+
+const productLine = document.querySelector('.finishOrder__container__right__form__summary__summaryBox__line')
+
+function createLine(){
+    const product = document.createElement('div')
+    product.classList.add('finishOrder__container__right__form__summary__summaryBox__line__product')
+
+    const spec = document.createElement('div')
+    spec.classList.add('finishOrder__container__right__form__summary__summaryBox__line__spec')
+
+    const price = document.createElement('div')
+    price.classList.add('finishOrder__container__right__form__summary__summaryBox__line__spec__price')
+
+    const minus = document.createElement('div')
+    minus.classList.add('finishOrder__container__right__form__summary__summaryBox__line__spec__munis')
+    minus.classList.add('primaryButton')
+    minus.setAttribute('href', '#')
+    minus.setAttribute('title', 'moins')
+
+    const quantity = document.createElement('div')
+    quantity.classList.add('finishOrder__container__right__form__summary__summaryBox__line__spec__quantity')
+
+    const more = document.createElement('div')
+    more.classList.add('finishOrder__container__right__form__summary__summaryBox__line__spec__more')
+    more.classList.add('primaryButton')
+    more.setAttribute('href', '#')
+    more.setAttribute('title', 'plus')
+
+    productLine.appendChild(product)
+    productLine.appendChild(spec)
+    spec.appendChild(price)
+    spec.appendChild(minus)
+    spec.appendChild(quantity)
+    spec.appendChild(more)
+}
