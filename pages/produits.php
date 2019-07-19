89a09658a71f00e7904b5313b36901fa8ce423b7
Romain Penchenat <romain.penchenat@gmail.com>
2019-07-19 19:46:21 +0200

    Fix bug CandideCollection

--- pages/produits.php
+++ pages/produits.php
@@ -34,7 +34,7 @@ $c5 = new CandideCollection('plats_cuisines');
                 ?>
                 <div class="prodSectionList__product  product all charcuterie" data-productid="<?php echo $id ?>" data-producttype="charcuterie">
                     <div class="prodSectionList__product__contain">
-                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
+                        <img src="<?php $c1->image("image_du_produit",$id,[900,600]) ?>" alt="image du produit">
                         <p><?php $c1->text('nom_du_produit',$id)?></p>
                         <p><?php $c1->text('prix',$id)?></p>
                         <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
@@ -49,7 +49,7 @@ $c5 = new CandideCollection('plats_cuisines');
                 ?>
                 <div class="prodSectionList__product product all porc" data-productid="<?php echo $id ?>" data-producttype="porc">
                     <div class="prodSectionList__product__contain">
-                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
+                        <img src="<?php $c2->image("image_du_produit",$id,[900,600]) ?>" alt="image du produit">
                         <p><?php $c2->text('nom_du_produit',$id)?></p>
                         <p><?php $c2->text('prix',$id)?></p>
                         <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
@@ -64,7 +64,7 @@ $c5 = new CandideCollection('plats_cuisines');
                 ?>
                 <div class="prodSectionList__product product all veau" data-productid="<?php echo $id ?>" data-producttype="veau">
                     <div class="prodSectionList__product__contain">
-                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
+                        <img src="<?php $c3->image("image_du_produit",$id,[900,600]) ?>" alt="image du produit">
                         <p><?php $c3->text('nom_du_produit',$id)?></p>
                         <p><?php $c3->text('prix',$id)?></p>
                         <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
@@ -79,7 +79,7 @@ $c5 = new CandideCollection('plats_cuisines');
                 ?>
                 <div class="prodSectionList__product product all boeuf" data-productid="<?php echo $id ?>" data-producttype="boeuf">
                     <div class="prodSectionList__product__contain">
-                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
+                        <img src="<?php $c4->image("image_du_produit",$id,[900,600]) ?>" alt="image du produit">
                         <p><?php $c4->text('nom_du_produit',$id)?></p>
                         <p><?php $c4->text('prix',$id)?></p>
                         <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
@@ -94,7 +94,7 @@ $c5 = new CandideCollection('plats_cuisines');
                 ?>
                 <div class="prodSectionList__product product all plats_cuisines" data-productid="<?php echo $id ?>" data-producttype="plats_cuisines">
                     <div class="prodSectionList__product__contain">
-                        <img src="../assets/images/photos/viande_boeuf_boveroux.jpg" alt="image du produit">
+                        <img src="<?php $c5->image("image_du_produit",$id,[900,600]) ?>" alt="image du produit">
                         <p><?php $c5->text('nom_du_produit',$id)?></p>
                         <p><?php $c5->text('prix',$id)?></p>
                         <a href="" class="primaryButton" id="whiteButton">Ajouter au panier</a>
