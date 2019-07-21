69f48a084a685f25393e13030406b2a2dbc32596
Tina Elh <tinaelha@gmail.com>
2019-07-21 18:39:56 +0200

    update html scss

--- pages/detailproduit.php
+++ pages/detailproduit.php
@@ -8,22 +8,22 @@ $productId = $_GET["id"];
 $productType = $_GET["type"];
 $c = new CandideCollectionItem ($productType,$productId);
 ?>
-<div class="containerDetailedProducts">
+<div class="containerDetailedProducts" data-id="<?php echo $productId ?>" data-type="<?php echo $productType ?>">
     <div class="containerDetailedProducts__informationsDetailed">
         <div class="containerDetailedProducts__informationsDetailed__imageDetailed">
-            <img src="<?php $c->image("image_du_produit",[900,600]) ?>" alt="photo de boeuf"> 
+            <img src="<?php $c->image("image_du_produit",[900,600]) ?>" alt="<?php $c->text("nom_du_produit")?> - Élevage Boveroux"> 
         </div>
         <div class="containerDetailedProducts__informationsDetailed__textDetailed">
-            <img class="closeDetailedProduct" src="../assets/images/icones/cross.svg" alt="croix"> 
             <div class="containerDetailedProducts__informationsDetailed__textDetailed__descriptionDetailed">
                 <h2><?php echo str_replace("_"," ",$productType) ?></h2>
                 <h3><?php $c->text("nom_du_produit")?></h3>
                 <p><?php $c->text("description_du_produit",false) ?></p>
-            </div>
-            <div class="containerDetailedProducts__informationsDetailed__textDetailed__buttonDetailed">
-                <a title="bouton ajouter panier" class="primaryButton" href="#">Ajouter au panier</a>
+        </div>
+        </div>
+        <img class="closeDetailedProduct" src="../assets/images/icones/cross.svg" alt="croix"> 
+        <div class="containerDetailedProducts__informationsDetailed__buttonDetailed">
+                <a title="Ajouter mon produit au panier" class="primaryButton buttonAddToBasket" href="#">Ajouter au panier</a>
               <!-- <a title="bouton ajouter panier" class="primaryButton" href="#">Test</a> --> 
-            </div>
         </div>
     </div>
 </div>
