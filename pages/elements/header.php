68a4ac7de09d64514231ecceafa8cef40177f862
Tina Elh <tinaelha@gmail.com>
2019-07-22 14:55:33 +0200

    début HTML SCSS menu button contents navbar

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -10,9 +10,29 @@
             </div>
             <div class="headerContainer__inside__cat__underButton">
                 <a href="" title="" class="secondaryButton">Mon Panier</a>
-                <div class="headerContainer__inside__cat__underButton__menuBasketEmpty">
+                <div class="headerContainer__inside__cat__underButton__menuBasketEmpty basketInvisible">
                     <h3>Mon panier</h3>
                     <p>Vous n’avez pas encore ajouter de produits à votre panier</p>
+                </div>
+                <div class="headerContainer__inside__cat__underButton__menuBasketContents basketInvisible">
+                    <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo">
+                        <h3>Mon panier</h3>
+                        <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo__priceDetailed">
+                            <p>Poitrine de veau</p>
+                            <p>29€</p>
+                        </div>
+                        <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo__priceDetailed">
+                            <p>Andouillettes</p>
+                            <p>25€</p>
+                        </div>
+                        <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo__priceDetailed">
+                            <p>Poulet rôti</p>
+                            <p>19€</p>
+                        </div>
+                    </div>
+                    <div class="headerContainer__inside__cat__underButton__menuBasketContents__buttonFinishOrder">
+                        <a title="Finaliser ma commande" class="primaryButton" href="#">Finaliser ma commande</a>
+                    </div>
                 </div>
             </div>
         </nav>
