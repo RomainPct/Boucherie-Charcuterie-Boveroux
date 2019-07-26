2cfc5f87b942861fec9bf70a09da22cedee985f5
Tina Elh <tinaelha@gmail.com>
2019-07-26 14:08:56 +0200

    remise au propre du submenu basketContents header HTML et SCSS

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -12,41 +12,40 @@
                 <a href="#" title="Mon panier" class="secondaryButton">Mon Panier</a>
                 <div class="headerContainer__inside__cat__underButton__menuBasketEmpty basketInvisible">
                     <h3>Mon panier</h3>
-                    <p>Vous n’avez pas encore ajouter de produits à votre panier</p>
-                </div>
-                <div class="headerContainer__inside__cat__underButton__menuBasketContents basketInvisible">
-                    <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo">
-                        <h3>Mon panier</h3>
-                        <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo__priceDetailed">
-                            <p>Poitrine de veau</p>
-                            <p>29€</p>
-                            <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
-                                <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
-                                <p>1</p>
-                                <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
+                    <p class="basketInvisible">Vous n’avez pas encore ajouter de produits à votre panier</p>
+                    <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents">
+                        <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo">
+                            <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
+                                <p>Poitrine de veau</p>
+                                <p>29€</p>
+                                <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
+                                    <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
+                                    <p>1</p>
+                                    <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
+                                </div>
+                            </div>
+                            <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
+                                <p>Andouillettes</p>
+                                <p>25€</p>
+                                <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
+                                    <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
+                                    <p>2</p>
+                                    <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
+                                </div>
+                            </div>
+                            <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
+                                <p>Poulet rôti</p>
+                                <p>19€</p>
+                                <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
+                                    <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
+                                    <p>1</p>
+                                    <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
+                                </div>
                             </div>
                         </div>
-                        <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo__priceDetailed">
-                            <p>Andouillettes</p>
-                            <p>25€</p>
-                            <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
-                                <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
-                                <p>2</p>
-                                <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
-                            </div>
+                        <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__buttonFinishOrder">
+                            <a title="Finaliser ma commande" class="primaryButton" href="#">Finaliser ma commande</a>
                         </div>
-                        <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo__priceDetailed">
-                            <p>Poulet rôti</p>
-                            <p>19€</p>
-                            <div class="headerContainer__inside__cat__underButton__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
-                                <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
-                                <p>1</p>
-                                <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="headerContainer__inside__cat__underButton__menuBasketContents__buttonFinishOrder">
-                        <a title="Finaliser ma commande" class="primaryButton" href="#">Finaliser ma commande</a>
                     </div>
                 </div>
             </div>
