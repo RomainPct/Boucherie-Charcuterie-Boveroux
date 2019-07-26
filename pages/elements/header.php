bb6da936a525a78e06adbfc5efee001b70147f48
Tina Elh <tinaelha@gmail.com>
2019-07-26 20:11:48 +0200

    Placement basketEmpty dans inside cat, modif alignement SCSS, rename classe basketIsEmpty

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -2,52 +2,52 @@
     <div class="headerContainer__inside">
         <a href="/" title="Accueil élevage Boveroux et fils" class="headerContainer__inside__logo"></a>
         <nav class="headerContainer__inside__cat subMenu">
-            <div class="headerContainer__inside__cat__under">
-                <a href="../quisommesnous">Qui sommes-nous ?</a>
+            <div class="headerContainer__inside__cat__menuBasketEmpty basketIsEmpty">
+                <h3>Mon panier</h3>
+                <p class="basketIsEmpty">Vous n’avez pas encore ajouter de produits à votre panier</p>
+                <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents">
+                    <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo">
+                        <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
+                            <p class="nameProduct">Poitrine de veau</p>
+                            <p class="priceOfProduct">29€</p>
+                            <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
+                                <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
+                                <p>1</p>
+                                <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
+                            </div>
+                        </div>
+                        <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
+                            <p class="nameProduct">Andouillettes</p>
+                            <p class= "priceOfProduct">25€</p>
+                            <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
+                                <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
+                                <p>2</p>
+                                <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
+                            </div>
+                        </div>
+                        <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
+                            <p class="nameProduct">Poulet rôti</p>
+                            <p class= "priceOfProduct">19€</p>
+                            <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
+                                <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
+                                <p>1</p>
+                                <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__buttonFinishOrder">
+                        <a href="../terminercommande.php" title="Finaliser ma commande" class="primaryButton">Finaliser ma commande</a>
+                    </div>
+                </div>
             </div>
             <div class="headerContainer__inside__cat__under">
-                <a href="../produits">Nos produits</a>
+                <a href="../quisommesnous" class="tertiaryButton">Qui sommes-nous ?</a>
+            </div>
+            <div class="headerContainer__inside__cat__under">
+                <a href="../produits" class="tertiaryButton">Nos produits</a>
             </div>
             <div class="headerContainer__inside__cat__underButton">
                 <a href="#" title="Mon panier" class="secondaryButton">Mon Panier</a>
-                <div class="headerContainer__inside__cat__underButton__menuBasketEmpty basketInvisible">
-                    <h3>Mon panier</h3>
-                    <p class="basketInvisible">Vous n’avez pas encore ajouter de produits à votre panier</p>
-                    <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents">
-                        <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo">
-                            <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
-                                <p>Poitrine de veau</p>
-                                <p>29€</p>
-                                <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
-                                    <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
-                                    <p>1</p>
-                                    <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
-                                </div>
-                            </div>
-                            <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
-                                <p>Andouillettes</p>
-                                <p>25€</p>
-                                <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
-                                    <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
-                                    <p>2</p>
-                                    <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
-                                </div>
-                            </div>
-                            <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
-                                <p>Poulet rôti</p>
-                                <p>19€</p>
-                                <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
-                                    <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
-                                    <p>1</p>
-                                    <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="headerContainer__inside__cat__underButton__menuBasketEmpty__menuBasketContents__buttonFinishOrder">
-                            <a title="Finaliser ma commande" class="primaryButton" href="#">Finaliser ma commande</a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </nav>
         <a href="#" class="secondaryButton menuButton">Menu</a>
--- src/scripts/9_showBasketMenu.js
+++ src/scripts/9_showBasketMenu.js
@@ -1,11 +1,11 @@
 let myBasket = document.querySelector("a.secondaryButton")
-let basketEmpty = document.querySelector(".headerContainer__inside__cat__underButton__menuBasketEmpty")
-let basketContents = document.querySelector(".headerContainer__inside__cat__underButton__menuBasketContents")
+let basketEmpty = document.querySelector(".headerContainer__inside__cat__menuBasketEmpty")
+let basketContents = document.querySelector(".headerContainer__inside__cat__menuBasketContents")
 
 myBasket.addEventListener('click', () =>{
-    if(basketEmpty.classList.contains("basketInvisible")){ 
-        basketEmpty.classList.remove("basketInvisible")
+    if(basketEmpty.classList.contains("basketIsEmpty")){ 
+        basketEmpty.classList.remove("basketIsEmpty")
     } else{
-        basketEmpty.classList.add("basketInvisible")
+        basketEmpty.classList.add("basketIsEmpty")
     }
 })
