64605d98a93a95b9ac9c64801aa0795565d4abc8
Romain Penchenat <romain.penchenat@gmail.com>
2019-07-27 16:14:08 +0200

    Help Hugo

--- src/scripts/3_categorieProduits.js
+++ src/scripts/3_categorieProduits.js
@@ -8,9 +8,18 @@ class Filter {
 
     init() {
         for (let i = 0; i < this.buttons.length; i++) {
-            this.buttons[i].addEventListener('click', () => {
-                let target = this.buttons[i].getAttribute('data-target');
 
+            this.buttons[i].addEventListener('click', () => {
+                // Gérer le style du bouton
+                var oldButton = document.querySelector('.prodSection__cat .secondaryButton')
+                oldButton.classList.remove('secondaryButton')
+                oldButton.classList.add('tertiaryButton')
+
+                this.buttons[i].classList.remove('tertiaryButton')
+                this.buttons[i].classList.add('secondaryButton')
+
+                // Gérer l'affichage des bonnes cartes
+                let target = this.buttons[i].getAttribute('data-target');
                 for (let j = 0; j < this.cards.length; j++) {
                     if (this.cards[j].classList.contains(target)) {
                         this.cards[j].style.display = 'block'
@@ -19,24 +28,10 @@ class Filter {
                     }
                 }
             })
+
         }
     }
 
 }
 
 let myFilter = new Filter()
-
-
-/*
-let container = document.querySelectorAll("containerButton")
-let button = document.querySelectorAll("a.primaryButton")
-
-button.forEach(function (button) {
-    button.addEventListener('click', function () {
-      container.classList.add('.remove');
-    })
-})
-console.log(container)
-*/
-
-//Gestion ajout au panier
