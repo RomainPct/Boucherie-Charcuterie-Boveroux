3a76f34e69068c31f839c236dc809afc62334d81
Romain Penchenat <romain.penchenat@gmail.com>
2019-07-28 23:29:21 +0200

    Gestion basket

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -2,41 +2,13 @@
     <div class="headerContainer__inside">
         <a href="/" title="Accueil élevage Boveroux et fils" class="headerContainer__inside__logo"></a>
         <nav class="headerContainer__inside__cat subMenu">
-            <div class="headerContainer__inside__cat__menuBasketEmpty hideContentBasket basketIsEmpty">
+            <div id="js_basket" class="headerContainer__inside__cat__menuBasketEmpty hideContentBasket basketIsEmpty">
                 <h3>Mon panier</h3>
                 <p class="textEmpty">Vous n’avez pas encore ajouter de produits à votre panier</p>
                 <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents">
-                    <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo">
-                        <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
-                            <p class="nameProduct">Poitrine de veau</p>
-                            <p class="priceOfProduct">29€</p>
-                            <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
-                                <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
-                                <p>1</p>
-                                <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
-                            </div>
-                        </div>
-                        <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
-                            <p class="nameProduct">Andouillettes</p>
-                            <p class= "priceOfProduct">25€</p>
-                            <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
-                                <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
-                                <p>2</p>
-                                <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
-                            </div>
-                        </div>
-                        <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed">
-                            <p class="nameProduct">Poulet rôti</p>
-                            <p class= "priceOfProduct">19€</p>
-                            <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo__priceDetailed__quantityProducts">
-                                <a href="#" title="Diminuer le nombre de produits" class="primaryButton">-</a>
-                                <p>1</p>
-                                <a href="#" title="Augmenter le nombre de produits" class="primaryButton">+</a>
-                            </div>
-                        </div>
-                    </div>
+                    <div id="js_basketContent" class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo"></div>
                     <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__buttonFinishOrder">
-                        <a href="../terminercommande.php" title="Finaliser ma commande" class="primaryButton">Finaliser ma commande</a>
+                        <a href="/terminercommande" title="Finaliser ma commande" class="primaryButton">Finaliser ma commande</a>
                     </div>
                 </div>
             </div>
