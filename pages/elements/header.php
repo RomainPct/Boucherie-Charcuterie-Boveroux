104b02e8bf15bb2847bda5a98d45d380c7794d15
Romain Penchenat <romain.penchenat@gmail.com>
2019-08-02 19:05:29 +0200

    Correctif header reponsive

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -2,16 +2,6 @@
     <div class="headerContainer__inside">
         <a href="/" title="Accueil élevage Boveroux et fils" class="headerContainer__inside__logo"></a>
         <nav class="headerContainer__inside__cat subMenu">
-            <div id="js_basket" class="headerContainer__inside__cat__menuBasketEmpty hideContentBasket basketIsEmpty">
-                <h3>Mon panier</h3>
-                <p class="textEmpty">Vous n’avez pas encore ajouter de produits à votre panier</p>
-                <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents">
-                    <div id="js_basketContent" class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo"></div>
-                    <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__buttonFinishOrder">
-                        <a href="/terminercommande" title="Finaliser ma commande" class="primaryButton">Finaliser ma commande</a>
-                    </div>
-                </div>
-            </div>
             <div class="headerContainer__inside__cat__under underHidden">
                 <a href="../accueil" class="tertiaryButton">Accueil</a>
             </div>
@@ -24,6 +14,16 @@
             <div class="headerContainer__inside__cat__underButton">
                 <div class="headerContainer__inside__cat__underButton__notif"></div>
                 <a href="#" title="Mon panier" class="secondaryButton">Mon Panier</a>
+            </div>
+            <div id="js_basket" class="headerContainer__inside__cat__menuBasketEmpty hideContentBasket basketIsEmpty">
+                <h3>Mon panier</h3>
+                <p class="textEmpty">Vous n’avez pas encore ajouter de produits à votre panier</p>
+                <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents">
+                    <div id="js_basketContent" class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo"></div>
+                    <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__buttonFinishOrder">
+                        <a href="/terminercommande" title="Finaliser ma commande" class="primaryButton">Finaliser ma commande</a>
+                    </div>
+                </div>
             </div>
         </nav>
         <a href="#" class="secondaryButton menuButton">Menu</a>
