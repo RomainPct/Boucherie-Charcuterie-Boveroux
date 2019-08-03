fcc3bad590d1b10d9f17739174fea6929d0feeb7
Tina Elh <tinaelha@gmail.com>
2019-08-03 11:32:48 +0200

    test changement de couleur screen pour le menu panier SCSS et JS

--- src/scripts/9_showBasketMenu.js
+++ src/scripts/9_showBasketMenu.js
@@ -1,12 +1,17 @@
 let myBasket = document.querySelector("a.secondaryButton")
 let basketEmpty = document.querySelector(".headerContainer__inside__cat__menuBasketEmpty")
 let basketContents = document.querySelector(".headerContainer__inside__cat__menuBasketContents")
+let body = document.querySelector("body")
 
 myBasket.addEventListener('click', (e) =>{
     e.preventDefault()
     if(basketEmpty.classList.contains("hideContentBasket")){ 
         basketEmpty.classList.remove("hideContentBasket")
+        body.classList.add("screenDarkMenu")
+        console.log(body)
     } else{
         basketEmpty.classList.add("hideContentBasket")
+        body.classList.remove("screenDarkMenu")
+        console.log(body)
     }
 })
