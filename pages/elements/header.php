88fd63e84b32ddbf6d47058fde49928a0df62fd2
Tina Elh <tinaelha@gmail.com>
2019-08-04 14:38:00 +0200

    écran noirci darkScreenMenu SCSS, HTML normalScreen, réglage ouverture fermeture écran noir JS sur desktop

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -1,32 +1,34 @@
 <header class="headerContainer">
-    <div class="headerContainer__inside">
-        <a href="/" title="Accueil élevage Boveroux et fils" class="headerContainer__inside__logo"></a>
-        <nav class="headerContainer__inside__cat subMenu">
-            <div class="headerContainer__inside__cat__under underHidden">
-                <a href="../accueil" class="tertiaryButton">Accueil</a>
-            </div>
-            <div class="headerContainer__inside__cat__under">
-                <a href="../quisommesnous" class="tertiaryButton">Qui sommes-nous ?</a>
-            </div>
-            <div class="headerContainer__inside__cat__under">
-                <a href="../produits" class="tertiaryButton">Nos produits</a>
-            </div>
-            <div class="headerContainer__inside__cat__underButton">
-                <div class="headerContainer__inside__cat__underButton__notif"></div>
-                <a href="#" title="Mon panier" class="secondaryButton">Mon Panier</a>
-            </div>
-            <div id="js_basket" class="headerContainer__inside__cat__menuBasketEmpty hideContentBasket basketIsEmpty">
-                <h3>Mon panier</h3>
-                <p class="textEmpty">Vous n’avez pas encore ajouter de produits à votre panier</p>
-                <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents">
-                    <div id="js_basketContent" class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo"></div>
-                    <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__buttonFinishOrder">
-                        <a href="/terminercommande" title="Finaliser ma commande" class="primaryButton">Finaliser ma commande</a>
+    <div class="normalScreen">
+        <div class="headerContainer__inside">
+            <a href="/" title="Accueil élevage Boveroux et fils" class="headerContainer__inside__logo"></a>
+            <nav class="headerContainer__inside__cat subMenu">
+                <div class="headerContainer__inside__cat__under underHidden">
+                    <a href="../accueil" class="tertiaryButton">Accueil</a>
+                </div>
+                <div class="headerContainer__inside__cat__under">
+                    <a href="../quisommesnous" class="tertiaryButton">Qui sommes-nous ?</a>
+                </div>
+                <div class="headerContainer__inside__cat__under">
+                    <a href="../produits" class="tertiaryButton">Nos produits</a>
+                </div>
+                <div class="headerContainer__inside__cat__underButton">
+                    <div class="headerContainer__inside__cat__underButton__notif"></div>
+                    <a href="#" title="Mon panier" class="secondaryButton">Mon Panier</a>
+                </div>
+                <div id="js_basket" class="headerContainer__inside__cat__menuBasketEmpty hideContentBasket basketIsEmpty">
+                    <h3>Mon panier</h3>
+                    <p class="textEmpty">Vous n’avez pas encore ajouter de produits à votre panier</p>
+                    <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents">
+                        <div id="js_basketContent" class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__productsInfo"></div>
+                        <div class="headerContainer__inside__cat__menuBasketEmpty__menuBasketContents__buttonFinishOrder">
+                            <a href="/terminercommande" title="Finaliser ma commande" class="primaryButton">Finaliser ma commande</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
-        <a href="#" class="secondaryButton menuButton">Menu</a>
-        <div class="greyOpacity hiddenOpac"></div>
+            </nav>
+            <a href="#" class="secondaryButton menuButton">Menu</a>
+            <div class="greyOpacity hiddenOpac"></div>
+        </div>
     </div>
 </header> 
--- src/scripts/9_showBasketMenu.js
+++ src/scripts/9_showBasketMenu.js
@@ -1,12 +1,14 @@
 let myBasket = document.querySelector("a.secondaryButton")
 let basketEmpty = document.querySelector(".headerContainer__inside__cat__menuBasketEmpty")
-let basketContents = document.querySelector(".headerContainer__inside__cat__menuBasketContents")
+let normalScreenMenu = document.querySelector(".normalScreen")
 
 myBasket.addEventListener('click', (e) =>{
     e.preventDefault()
     if(basketEmpty.classList.contains("hideContentBasket")){ 
         basketEmpty.classList.remove("hideContentBasket")
-    } else{
+        normalScreenMenu.classList.add("darkScreenMenu")
+    } else{ 
         basketEmpty.classList.add("hideContentBasket")
+        normalScreenMenu.classList.remove("darkScreenMenu")
     }
 })
