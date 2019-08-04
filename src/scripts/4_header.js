3d73e77d8e4091e389cbe1c2600f698ae2280b7b
Tina Elh <tinaelha@gmail.com>
2019-08-04 14:50:56 +0200

    En responsive ajout darkScreenMenu sur le menu ouvert et disparition quand menu fermé

--- src/scripts/4_header.js
+++ src/scripts/4_header.js
@@ -2,6 +2,7 @@ var menuBtn = document.querySelector('.menuButton')
 var subMenu = document.querySelector('.subMenu')
 var greyOpacity = document.querySelector('.greyOpacity')
 var basketHidden = document.querySelector('.headerContainer__inside__cat__menuBasketEmpty')
+var normalScreen = document.querySelector(".normalScreen")
 
 menuBtn.addEventListener(
     'click',
@@ -9,11 +10,13 @@ menuBtn.addEventListener(
         if(subMenu.classList.contains('visible') == true){
             subMenu.classList.remove('visible')
             greyOpacity.classList.remove('visible')
+            normalScreen.classList.remove("darkScreenMenu")
             menuBtn.innerHTML = "Menu"
         } else {
             subMenu.classList.add('visible')
             greyOpacity.classList.add('visible')
             basketHidden.classList.remove('hideContentBasket')
+            normalScreen.classList.add("darkScreenMenu")
             menuBtn.innerHTML = "Fermer"
         }
     }
