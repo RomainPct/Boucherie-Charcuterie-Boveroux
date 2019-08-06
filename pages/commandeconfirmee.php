e10723ee8294ada8bff9c6ab6eaf1726084e40ef
louplemaire <44206840+louplemaire@users.noreply.github.com>
2019-08-06 12:34:13 +0200

    add comingFade in commandeconfirmee

--- pages/commandeconfirmee.php
+++ pages/commandeconfirmee.php
@@ -1,5 +1,5 @@
 <?php include 'php/sendOrder.action.php' ?>
-<main class="confirmedOrder__container">
+<main class="confirmedOrder__container comingFade">
     <h2>Commande envoyée</h2>
     <p>Merci de nous avoir fait confiance, une réponse vous sera transmise d’ici 48h</p>
     <a class="secondaryButton" href="accueil" title="Revenir à l'accueil">Revenir à l'accueil</a>
